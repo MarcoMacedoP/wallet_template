b989ddd7ab6b0419951e57395dc564de8c69d2b7
Marco <marcosiegman01@gmail.com>
2020-02-06 16:58:29 -0600

    added styled-components

--- App.tsx
+++ App.tsx
@@ -10,9 +10,7 @@ const App = () => {
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <ScrollView contentInsetAdjustmentBehavior="automatic">
-          <HomeComponent />
-        </ScrollView>
+        <HomeComponent />
       </SafeAreaView>
     </>
   );
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
-import {Text} from 'react-native';
+import styled from 'styled-components/native';
 
 export const HomeComponent = () => {
-  return <Text> Hello, world 👨‍🌾 </Text>;
+  return (
+    <Container>
+      <Title> Hello, world 👨‍🌾</Title>
+    </Container>
+  );
 };
+
+const Container = styled.View`
+  padding: 22px;
+  height: 100%;
+  width: 100%;
+  justify-content: center;
+`;
+const Title = styled.Text`
+  font-size: 36px;
+  font-weight: bold;
+`;
