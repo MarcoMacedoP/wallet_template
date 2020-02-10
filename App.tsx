4c47769ece8dd8bdcdf9fbe692d8cb1d7e4eecfb
Marco <marcosiegman01@gmail.com>
2020-02-10 10:30:57 -0600

    xd

--- App.tsx
+++ App.tsx
@@ -13,7 +13,7 @@ import {colors} from './src/shared/styles/variables';
 
 //declarations
 declare var global: {HermesInternal: null | {}};
-const {Navigator, Screen} = createStackNavigator({Home: BalanceScreen});
+const {Navigator, Screen} = createStackNavigator();
 
 const homeOptions: StackNavigationOptions = {
   title: 'Wallet',
