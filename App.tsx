b0ed2605a9ee7db6e9337f75aa230f4727405d3e
Marco <marcosiegman01@gmail.com>
2020-02-14 11:30:53 -0600

    added nice loading

--- App.tsx
+++ App.tsx
@@ -1,26 +1,29 @@
 import 'react-native-gesture-handler';
 import './shim';
 import erc20 from 'erc20-wallet';
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import Router from './src/Router';
 import {useGlobalState} from 'globalState';
+import {Text} from 'shared/styled-components';
 
 //declarations
 declare var global: {HermesInternal: null | {}};
 
 const App = () => {
+  const [isLoading, setLoading] = useState(true);
   const [, setSeed] = useGlobalState('seed');
   const [, setToken] = useGlobalState('token');
 
   useEffect(() => {
     const callSeed = async () => {
       const seed = await erc20.createSeed();
+      setLoading(false);
       setSeed(seed);
       setToken(true);
     };
     callSeed();
   }, []);
 
-  return <Router />;
+  return isLoading ? <Text>Loading ...</Text> : <Router />;
 };
 export default App;
